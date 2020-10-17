33173dd72ca6130d75d94bf0a8252c81cf2552f9
TheRook <firealwaysworks@gmail.com>
2020-10-16 21:04:32 -0700

    adding tokens for the InstToken base class

---
+++ Instant/contracts/inst.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.6.2;
+import 'contracts/InstToken.sol';
+
+abstract contract instcoin is InstToken {
+    using SafeMath for uint256;
+
+    // modify token name
+    string public constant NAME = 'Instant';
+    // modify token symbol
+    string public constant SYMBOL = 'in.st';
+    // modify token decimals
+    uint8 public constant DECIMALS = 6;
+    // modify initial token supply
+    uint256 public constant INITIAL_SUPPLY = 300000000 * (10**uint256(DECIMALS)); // 300,000,000 tokens
+}
