71a5fca8755ecc33a9abde32fd620e0a9b82ebe8
TheRook <firealwaysworks@gmail.com>
2020-10-16 21:03:54 -0700

    builds

--- Instant/contracts/InstToken.sol
+++ Instant/contracts/InstToken.sol
@@ -16,24 +16,12 @@ abstract contract IERC223Recipient {
     function tokenFallback(address _from, uint _value, bytes memory _data) virtual public;
 }
 
-
 /**
  * @title InstToken
  * @dev Simple ERC20 Token with freezing and blacklist
  */
 
-contract InstToken is ERC20, Ownable {
-    using SafeMath for uint256;
-
-    // modify token name
-    string public constant NAME = 'Instant';
-    // modify token symbol
-    string public constant SYMBOL = 'in.st';
-    // modify token decimals
-    uint8 public constant DECIMALS = 6;
-    // modify initial token supply
-    uint256 public constant INITIAL_SUPPLY = 300000000 * (10**uint256(DECIMALS)); // 300,000,000 tokens
-
+abstract contract InstToken is ERC20, Ownable {
     // indicate if the token is freezed or not
     bool public freezed;
 
@@ -45,15 +33,6 @@ contract InstToken is ERC20, Ownable {
     event AddedTodenylist(address[] addrs);
     event RemovedFromdenylist(address[] addrs);
 
-    /**
-     * @dev Constructor that gives msg.sender all of existing tokens.
-     */
-    constructor() public Ownable() ERC20(NAME, SYMBOL) {
-        _setupDecimals(DECIMALS);
-        _mint(msg.sender, INITIAL_SUPPLY);
-        emit Transfer(address(0x0), msg.sender, INITIAL_SUPPLY);
-        freezed = true;
-    }
 
     /**
      * @dev freeze and unfreeze functions
